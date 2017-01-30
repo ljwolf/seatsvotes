fix vote differential computation and district sensitivity report

politipy/seatsvotes/estimators.py
@@ -228,44 +228,52 @@ def pairwise_attainment_gap(obs_turnout, obs_shares, swing_ratios, rule=ut.plura
     biases = np.subtract.outer(extrap, extrap)
     return np.triu(biases), extrap
 
-def directed_waste(district_votes, turnout, names=None):
+def directed_waste(voteshares, turnout=None):
     """
-    Compute the directed waste in votes over districts in a two-party system
+    Compute the directed waste in votes over districts in a two-party system. 
+    Expresses the percentage of all votes wasted for the reference party over and above the other party. 
+    
+    If negative, indicates bias against the reference party, in that more votes are wasted by the reference party than by the opponent.
 
     Arguments
     ----------
-    district_votes      :   np.ndarray (n,p)
-                        vector of raw votes cast for `p` parties in `n` districts. If a two-party system, this may be (n,1), with the remaining opposition votes coming from `turnout - district_votes`
-    turnout             :   np.ndarray (n,1)
-                        vector of raw votes cast for all parties in `n` districts
-    """
-    turnout = np.asarray(turnout).reshape(-1,1)
-    if isinstance(district_votes, (pd.DataFrame)):
-        names = district_votes.columns if names is None else Names
-        district_votes = district_votes.values
-    elif isinstance(district_votes, pd.Series):
-        if names is None:
-            names = (district_votes.name, 'not ' + district_votes.name)
-        district_votes = district_votes.values.reshape(-1,1)
-    N,p = district_votes.shape
-    if ((0 < district_votes) & (district_votes < 1)).all():
-        district_votes = district_votes *  turnout
-    if p == 1:
-        p = 2 #two party info can be passed as one vector
-        district_votes = np.hstack((district_votes, turnout - district_votes))
-    names = np.arange(p) if names is None else names
-    decisive = np.ceil(turnout * (1/p))
-    waste = district_votes - decisive
-    total_waste = []
-    for party_waste in waste.T:
-        this_waste = np.sum(party_waste[party_waste > 0])
-        total_waste.append(this_waste)
-    disparities = np.subtract.outer(total_waste, total_waste)
-    return pd.DataFrame(np.triu(disparities), columns=names, index=names)
+    voteshares  :   np.ndarray (n,1)
+                    Voteshares for the reference party against which the 
+                    measure is computed. 
+    turnout     :   np.ndarray (n,1)
+                    vector of raw votes cast for all parties in `n` districts
+    
+    This is the McGhee (2014) measure of waste, where waste is defined by the 
+    sum of a party's surplus and losing votes. So, waste for one party is:
 
-def efficiency_gap(district_votes, turnout, names=None):
+    Sum  (votes in districts where party voteshare < .5)
+         + ((votes in districts where party voteshare > .5) 
+           - (.5 * turnout in districts where party voteshare > .5))
+    """
+    voteshares = np.asarray(voteshares).reshape(-1,1)
+    if turnout is None:
+        turnout = np.ones_like(voteshares)
+    turnout = np.asarray(turnout).reshape(voteshares.shape)
+    if not ((0 <= voteshares) & (voteshares <= 1)).all():
+        if ((0 <= voteshares) & (voteshares <= 100)).all():
+            voteshares /= 100
+        else:
+            raise Exception('Vote Shares must be between 0 and 1 with no NaN')
+    # waste is 
+    waste_for = ( ((voteshares > .5) * ((voteshares - .5) * turnout))
+                 +((voteshares < .5) * (voteshares * turnout))).sum()
+    waste_against = ( ((voteshares < .5) * ((voteshares - .5) * turnout))
+                     +((voteshares > .5) * (voteshares * turnout))).sum()
+    return waste_against - waste_for
+
+def efficiency_gap(voteshares, turnout=None):
     """
     compute the efficiency gap, which is defined as the directed waste divided by the total votes cast in an election
     """
-    dw = directed_waste(district_votes, turnout, names=names)
-    return dw/np.sum(turnout)
+    if turnout is None:
+        sbar = (voteshares > .5).mean()
+        hbar = voteshares.mean()
+        return (sbar - .5) - 2 * (hbar - .5)
+    else:
+        dw = directed_waste(voteshares, turnout)
+        return dw/np.sum(turnout)

politipy/seatsvotes/gelmanking/abstracts.py
@@ -531,7 +531,9 @@ class SeatsVotes(object):
 
         G_a = (\sum_i^b \min_j^n({v_{ij} | s_{ij} > .5}) / b) - .5
 
-        Where b is a number of simulation batches, n is the number of simulations-per-batch, v_{ij} is the reference paty vote share in batch j for replication i, s_{ij} is the reference party seat share in batch j for replication i.
+        Where b is a number of simulation batches, n is the number of simulations-per-batch, 
+        v_{ij} is the reference paty vote share in batch j for replication i, 
+        s_{ij} is the reference party seat share in batch j for replication i.
         """
         if best_target is None:
             try:
@@ -555,7 +557,7 @@ class SeatsVotes(object):
             agaps.append(candidate - .5)
         return np.percentile(agaps, q=q)
 
-    def get_efficiency_gap(self, t=-1, year=None, names=None):
+    def get_efficiency_gap(self, t=-1, year=None, names=None, use_turnout=True):
         """
         Compute the percentage difference of wasted votes in a given election
 
@@ -568,15 +570,21 @@ class SeatsVotes(object):
         Where V_{ik} is the raw vote cast in district i for party k and m_i is the total number of votes cast for all parties in district i
         """
         turnout, voteshares, a, b, c = self._extract_election(t=t,year=year)
-        return est.efficiency_gap(voteshares*turnout, turnout, names=names).iloc[0,1]
+        if use_turnout:
+            return est.efficiency_gap(voteshares, 
+                                      turnout)
+        else:
+            return est.efficiency_gap(voteshares, turnout=None)
 
     def estimate_efficiency_gap(self, t=-1, year=None, names=None,
                                 Xhyp=None, predict=False, n_sims=1000,
-                                q=[5,50,95]):
+                                q=[5,50,95], use_turnout=True):
         """
         Compute the efficiency gap expectation over many simulated elections. This uses the same estimator as `get_efficiency_gap`, but computes the efficiency gap over many simulated elections.
         """
         turnout, *rest = self._extract_election(t=t, year=year)
+        if not use_turnout:
+            turnout = None
         sims = self.simulate_elections(t=t,  Xhyp=Xhyp, predict=predict, n_sims=n_sims)
         gaps = [est.efficiency_gap(sim.reshape(-1,1), turnout=turnout,
                                    names=names).iloc[0,1]
@@ -637,7 +645,6 @@ class SeatsVotes(object):
                 mod.model.weights = np.delete(mod.model.weights, i)
                 del_models.append(mod)
         rstats = []
-        full_agap = self.get_attainment_gap(t=t)
         full_mbon = self.estimate_median_bonus(t=t, Xhyp=Xhyp)
         full_obon = self.estimate_observed_bonus(t=t, Xhyp=Xhyp)
         full_egap = self.estimate_efficiency_gap(t=t, Xhyp=Xhyp)
@@ -645,16 +652,17 @@ class SeatsVotes(object):
         for idx,mod in enumerate(del_models):
             self.models[t] = mod
             Xhyp_i = np.delete(Xhyp, idx, axis=0) if Xhyp is not None else None
-            agap = self.get_attainment_gap(t=t)
-            obs_egap = self.get_efficiency_gap(t=t)
+            obs_egap_t = self.get_efficiency_gap(t=t)
+            obs_egap_not = self.get_efficiency_gap(t=t, use_turnout=False)
             mbon = self.estimate_median_bonus(t=t, Xhyp=Xhyp_i, n_sims=n_sims)
             obon = self.estimate_observed_bonus(t=t, Xhyp=Xhyp_i, n_sims=n_sims)
             egap = self.estimate_efficiency_gap(t=t, Xhyp=Xhyp_i, n_sims=n_sims)
+            egap_not = self.estimate_efficiency_gap(t=t, Xhyp=Xhyp_i, n_sims=n_sims, use_turnout=False)
             rstats.append(np.hstack((agap, obs_egap, mbon, obon, egap)))
         self.models[t] = original
         rstats = np.vstack(rstats)
-        cols = ( ['AGap_e', 'EGap_e']
-                +['{}_{}'.format(name,ptile) for name in ['MBonus', 'OBonus', 'EGap']
+        cols = ( ['EGap_eT', 'EGap_enoT']
+                +['{}_{}'.format(name,ptile) for name in ['MBonus', 'OBonus', 'EGap_T', 'EGap_noT']
                  for ptile in (5,50,95)] )
         rstats = pd.DataFrame(rstats, columns=cols)
         leverage = pd.DataFrame(np.hstack((np.diag(leverage).reshape(-1,1), 