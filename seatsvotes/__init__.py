81d4261af5fff233831d5e60b437c83b04a0f0ea
ljwolf <levi.john.wolf@gmail.com>
2018-08-08 18:38:01 -0700

    remove now-missing moment_match from init

--- seatsvotes/__init__.py
+++ seatsvotes/__init__.py
@@ -1,7 +1,6 @@
 from .gelmanking.abstracts import SeatsVotes as SuccessiveOLS
 from .linzer.abstracts import SeatsVotes as GaussianMixture
 from .bootstrap.abstracts import SeatsVotes as Bootstrapper
-from .moment_match.abstracts import SeatsVotes as MomentMatch
 from .mcgann.abstracts import SeatsVotes as PanelOLS
 from .ecdf import Empirical_CDF as Empirical
 from . import data
