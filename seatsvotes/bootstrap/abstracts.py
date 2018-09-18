a64008595ca691e1934742e3c99a8f2fca66c177
ljwolf <levi.john.wolf@gmail.com>
2018-09-18 23:51:39 +0100

    fix nas in the bootstrap code

--- seatsvotes/bootstrap/abstracts.py
+++ seatsvotes/bootstrap/abstracts.py
@@ -7,8 +7,9 @@ class Bootstrap(Preprocessor, AlwaysPredictPlotter, AdvantageEstimator):
     def __init__(self, elex_frame, covariate_columns=None,
                  weight_column=None,
                  share_column='vote_share',
-                 year_column='year', redistrict_column=None, district_id='district_id',
-                 missing='drop', uncontested=None):
+                 year_column='year',
+                 redistrict_column=None, district_id='district_id',
+                 missing='ignore', uncontested='ignore'):
         super().__init__(elex_frame,
                          covariates=covariate_columns,
                          weight_column=weight_column,
@@ -25,7 +26,7 @@ class Bootstrap(Preprocessor, AlwaysPredictPlotter, AdvantageEstimator):
     def years(self):
         return self._years
 
-    def simulate_elections(self, n_sims=10000, predict=True,
+    def simulate_elections(self, n_sims=1000, predict=True,
                            t=-1, year=None, swing=0, target_v=None, fix=False):
         """
         Simulate elections according to a bootstrap technique. 
@@ -67,19 +68,19 @@ class Bootstrap(Preprocessor, AlwaysPredictPlotter, AdvantageEstimator):
                                       weights=this_year.weight)
         if predict is False:
             self._GIGO("Prediction must be true if using bootstrap")
-            target_h = this_year.vote_share
+            target_h = this_year.vote_share.values.flatten()
         else:
-            target_h = this_year.vote_share__prev
+            target_h = this_year.vote_share__prev.values.flatten()
         if swing is not None and target_v is not None:
             raise ValueError("either swing or target_v, not both.")
         elif target_v is not None:
             swing = (target_v - party_voteshares)
         obs_swings = (this_year.vote_share - this_year.vote_share__prev)
+        obs_swings = obs_swings.fillna(obs_swings.mean())
         n_dists = len(target_h)
-        pweights = (this_year.weight / this_year.weight.sum()).values
-        pweights = pweights[~obs_swings.isnull().values]
+        pweights = (this_year.weight / this_year.weight.sum()).values.flatten()
         pweights /= pweights.sum()
-        sim_swings = np.random.choice(obs_swings.dropna() + swing, (n_sims, n_dists),
+        sim_swings = np.random.choice(obs_swings + swing, (n_sims, n_dists),
                                       replace=True, p=pweights)
-        sim_h = target_h.values[None, :] + sim_swings
-        return np.clip(sim_h, 0,1)
+        sim_h = target_h[None, :] + sim_swings
+        return np.clip(sim_h, 0, 1)
