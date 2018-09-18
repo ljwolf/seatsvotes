518ef5aac06e40eb0eb5421a3c82168b4d46a606
ljwolf <levi.john.wolf@gmail.com>
2018-09-18 15:23:17 +0100

    add osf bridge for geodata in 1990s and 2000s

--- seatsvotes/data/__init__.py
+++ seatsvotes/data/__init__.py
@@ -1,6 +1,7 @@
 import os
 from collections import namedtuple
 import numpy as np
+import logging
 
 self_dir = os.path.dirname(__file__)
 
@@ -8,11 +9,77 @@ self_dir = os.path.dirname(__file__)
 def illinois():
     """ illinois elections to congress, including boundaries"""
     import geopandas as gpd
-    return gpd.read_file('zip://' + os.path.join(self_dir, 'illinois.zip'))
+    return gpd.read_file('tar://' + os.path.join(self_dir, 'illinois.zip'))
 
+def congress(state=None, geo=False, overwrite_cache=False):
+    """Elections to the house from ICPSR 6311
 
-def congress(state=None):
-    """Elections to the house from ICPSR 6311"""
+    Arguments
+    ----------
+    state       :   string (default: None)
+                    name of state to filter the data. all data is still
+                    read into memory. 
+    geo         :   bool   (default: False)
+                    whether or not to use the geographies of the congressional
+                    districts, provided by osf.io/vf9pf. Will be cached for 
+                    future use if requested. 
+    overwrite_cache:bool   (default: False)
+                    if the spatial data is requested this will force the cached
+                    version to be updated from the OSF if true. Otherwise,
+                    the cached version will be read. 
+    Returns
+    -------
+    (geo)dataframe containing election results, possibly with congressional
+    district shapes (if and only if geo=True)
+    """
+    if not geo:
+        return icpsr6311(state=state)
+    else:
+        _maybe_fetch_geodata_from_osf(overwrite_cache=overwrite_cache)
+        return _load_geodata(state=state)
+            
+def _load_geodata(state=None):
+    """ load the geodata from file, possibly filtering by a state"""
+    try:
+        import geopandas
+    except ImportError:
+        raise ImportError("package `geopandas` is required to use spatial data examples")
+    path = os.path.join(self_dir, 'wolf_2018_scidata-osf-vf9pf.tar.gz')
+    if not os.path.exists(path):
+        path = _fetch_geodata_from_osf()
+    full = geopandas.read_file('tar://' + path)
+    if state is not None:
+        return full.query('state_name == {}'.format(state.lower()))
+    return full
+    
+def _maybe_fetch_geodata_from_osf(overwrite_cache=False):
+    """ load the geodata from the open science framework, possibly overwriting
+    the version cached on the hard drive
+    """
+    fh = os.path.join(self_dir, 'wolf_2018_scidata-osf-vf9pf.tar.gz')
+    if os.path.exists(fh):
+        if not overwrite_cache:
+            return fh
+    try:
+        import osfclient
+    except ImportError:
+        raise ImportError("package `osfclient` is required to fetch spatial"
+                          " congressional district data from the open "
+                          " science framework")
+    cxn = osfclient.OSF()
+    wolf_scidata_storage = cxn.project('vf9pf').storage()
+    files = list(wolf_scidata_storage.files)
+    fnames = [f.name for f in files]
+    file_ix = fnames.index("nineties_and_aughties.tar.gz")
+    target_file = files[file_ix]
+    logging.info('starting download from OSF.')
+    with open(fh, 'wb') as f:
+        target_file.write_to(f)
+    logging.info('download from OSF successful.')
+    return fh
+
+def icspr6311(state=None):
+    """ pull elections data from ICPSR6311, grabbed from judgeitII R package"""
     import pandas as pd
     house6311 = pd.read_csv(os.path.join(self_dir, 'judgeit/house6311.csv'))
     house6311.columns = ['year', 'idx', 'state',
@@ -21,9 +88,8 @@ def congress(state=None):
         return house6311.query('state == {}'.format(state.lower()))
     return house6311
 
-
 def president(year=2012):
-    """ Elections to the presidency by congressional district, from JudgeIt"""
+    """ Elections to the presidency by congressional district, from judgeitII R package"""
     import pandas as pd
     if isinstance(year, int):
         if year > 2012:
@@ -54,7 +120,7 @@ def president(year=2012):
 
 
 def canada():
-    """ Canadian parliamentary elections in 1979, from Linzer 2012"""
+    """ Canadian parliamentary elections in 1979, from Linzer (2012)"""
     import pandas as pd
     out = pd.read_csv(os.path.join(self_dir, 'canada1979.csv'))
     out.columns = ['name', 'turnout', 'liberal',
